fc72f4cb0a325ab7a3030e9cefbd9dc19286633c
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-03-19 00:40:55 +0100

    Moved seed URLs to config file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,16 @@
 import abwcf.actors.Crawler
+import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.typed.ActorSystem
 
-val seedUrls = Seq("https://www.oth-regensburg.de/")
+import scala.jdk.CollectionConverters.*
 
 @main def startCrawler(): Unit = {
   val actorSystem = ActorSystem(Crawler(), "crawler")
+  
+  val seedUrls = ConfigFactory.load("seed-urls")
+    .getStringList("seed-urls")
+    .asScala
+    .toSeq
 
   actorSystem ! Crawler.SeedUrls(seedUrls)
 }
