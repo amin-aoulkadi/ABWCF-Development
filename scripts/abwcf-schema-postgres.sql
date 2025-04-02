dee8fbf310cea4fbaf4cbb3fc44c7c71a440f39e
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-04-02 22:24:12 +0200

    Added crawl priority

--- scripts/abwcf-schema-postgres.sql
+++ scripts/abwcf-schema-postgres.sql
@@ -1,7 +1,10 @@
 CREATE TABLE pages (
-  url         TEXT,
-  status      TEXT,
-  crawl_depth INTEGER
+  url            TEXT,
+  status         TEXT,
+  crawl_depth    INTEGER,
+  crawl_priority INTEGER
 );
 
-CREATE INDEX pages_id_index ON pages USING HASH (url); -- The ABWCF executes many queries with "... WHERE url = ?".
+-- Indexes to speed up common ABWCF queries:
+CREATE INDEX pages_id_index ON pages USING hash (url); -- For queries with "WHERE url = ?".
+CREATE INDEX pages_crawl_priority_index ON pages USING btree (crawl_priority); -- For queries with "ORDER BY crawl_priority".
