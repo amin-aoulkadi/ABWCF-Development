bc46ee51fc9d991f2868bf14f413edc516143238
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-04-07 20:37:20 +0200

    Added API for user-defined code to UserCodeRunner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,29 @@
 import abwcf.actors.Crawler
-import abwcf.util.{CrawlerSettings, PrioritizationFunctions}
+import abwcf.data.{FetchResponse, Page, PageCandidate}
+import abwcf.util.{CrawlerSettings, PrioritizationFunctions, UserCode}
 import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.actor.typed.scaladsl.ActorContext
+import org.apache.pekko.http.scaladsl.model.StatusCode
 
 import scala.jdk.CollectionConverters.*
 
 @main def startCrawler(): Unit = {
-  val settings = CrawlerSettings().withPrioritizationFunction(PrioritizationFunctions.Random)
+  val userCode = new UserCode {
+    override def prioritize(candidate: PageCandidate, context: ActorContext[_]): Long =
+      PrioritizationFunctions.random
+
+    override def onFetchSuccess(page: Page, response: FetchResponse, context: ActorContext[_]): Unit =
+      context.log.info("Processing page {} ({}, {} bytes)", page.url, response.status, response.body.length)
+
+    override def onFetchRedirect(page: Page, statusCode: StatusCode, redirectTo: Option[String], context: ActorContext[_]): Unit =
+      context.log.info("Processing redirect from {} ({}, redirection to {})", page.url, statusCode, redirectTo)
+
+    override def onFetchError(page: Page, statusCode: StatusCode, context: ActorContext[_]): Unit =
+      context.log.info("Processing error from {} ({})", page.url, statusCode)
+  }
+
+  val settings = CrawlerSettings(userCode)
   val actorSystem = ActorSystem(Crawler(settings), "crawler")
 
   val seedUrls = ConfigFactory.load("seed-urls")
