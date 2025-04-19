83a25029cb8e7fa6a067d2705d034bd1bd180c9c
amin-aoulkadi <65405621+amin-aoulkadi@users.noreply.github.com>
2025-04-19 22:19:31 +0200

    Implemented custom persistence for HostInformation

--- scripts/abwcf-schema-postgres.sql
+++ scripts/abwcf-schema-postgres.sql
@@ -5,6 +5,15 @@ CREATE TABLE pages (
   crawl_priority BIGINT
 );
 
+CREATE TABLE hosts (
+  scheme_and_authority TEXT,
+  robot_rules          TEXT,
+  crawl_delay          BIGINT,
+  valid_until          TIMESTAMP WITH TIME ZONE
+);
+
 -- Indexes to speed up common ABWCF queries:
 CREATE INDEX pages_id_index ON pages USING hash (url); -- For queries with "WHERE url = ?".
 CREATE INDEX pages_crawl_priority_index ON pages USING btree (crawl_priority); -- For queries with "ORDER BY crawl_priority".
+
+CREATE INDEX hosts_id_index ON hosts USING hash (scheme_and_authority); -- For queries with "WHERE scheme_and_authority = ?".
